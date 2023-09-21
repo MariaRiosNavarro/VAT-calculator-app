139f16c8427f9410ac45ebaf0e877f2e2d3cea3c
SuperCoder <propscode254@gmail.com>
2023-09-21 11:30:41 +0200

    create Plan

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// * Vat Calculator
+
+// - 1 Get data from all the inputs
+// - Define variables to store input values
+
+// - 1.A Save the direction of the function (OptionA: Gross to net) or (OptionB: net to gross).
+// - (OptionA: Brutto->Netto) or (OptionB: Netto->Brutto)
+
+// - 2: Define output
+// - Define a variable to store the calculated result
+
+// - 2.A Save the labels of the Output to change if to the option B is selected (Default A)
+
+// - 3: Create the function to calculate VAT.
+// - Get values from inputs
+
+// - 4: Error handling: Check if inputs are valid & Do all the inputs have data? Add outputs if not.
+
+// - 5: Calculate VAT
+
+// - 6: Reset the input when the focus is on it
