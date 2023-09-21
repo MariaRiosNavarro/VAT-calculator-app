e9258b89838de57e7c43bdd3a56ca4c02d26bad1
SuperCoder <propscode254@gmail.com>
2023-09-21 12:09:05 +0200

    fix text

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,8 @@
 // - 1 Get data from all the inputs
 // - Define variables to store input values
 
-// - 1.A Save the direction of the function (OptionA: Gross to net) or (OptionB: net to gross).
-// - (OptionA: Brutto->Netto) or (OptionB: Netto->Brutto)
+// - 1.A Save the direction of the function  (OptionA: net to gross(Default)) or (OptionB: Gross to net).
+// -(OptionA: Netto->Brutto (default)) (OptionB: Brutto->Netto)
 
 // - 2: Define output
 // - Define a variable to store the calculated result
@@ -20,7 +20,7 @@
 
 // - 6: Reset the input when the focus is on it
 
-// ADD Text of Labels for OptionB: net to gross (default A)
+// ADD Text of Labels for OptionB(gross->net)    (default is A)
 
-// for data-js="inputNumberLabel" = `Nettobetrag(Preis ohne Mehrwersteuer) in Euro <span class="red">*</span>`
-// for data-js="outputB-label" = `Bruttobetrag(Endpreis)`;
+// for data-js="inputNumberLabel" = `Bruttobetrag (Preis inklusive Mehrwersteuer) in Euro <span class="red">*</span>`
+// for data-js="outputB-label" = `Nettobetrag(Endpreis)`;
